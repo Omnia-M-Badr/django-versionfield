de2044e719039d8042cf2c57eedeaa239e13cc76
carderm <michaelcard_GitHub@outlook.com>
2020-05-23 14:53:19 +1000

    Bug fixes.

--- versionfield/__init__.py
+++ versionfield/__init__.py
@@ -1,6 +1,6 @@
 from .fields import VersionField
 from .version import Version
 
-default_app_config = "django_changes.apps.DjangoChangesConfig"
+default_app_config = "versionfield.apps.VersionFieldConfig"
 
 
